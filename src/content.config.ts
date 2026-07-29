5870853ff6da6b91bfa46ac432de932c44bbf768
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2026-07-28 22:45:48 -0400

    refactor: rename blog to collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,9 +2,9 @@ import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+const collection = defineCollection({
+  // Load Markdown and MDX files in the `src/content/collection/` directory.
+  loader: glob({ base: "./src/content/collection", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: ({ image }) =>
     z.object({
@@ -20,4 +20,4 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+export const collections = { collection };
